4a5fb7fe6369c491ce9e77ce5dc9468b01bcb17d
freetimezp <kraftukrnet@ukr.net>
2021-07-04 16:38:07 +0300

    phpbasic 3

--- index.php
+++ index.php
@@ -1,54 +1,43 @@
 <?php
 
-/*lesson 1*/
-
-$a = 1; //integer
-$b = 2;
-echo $a + $b . '<br>';
-var_dump($a+$b);
-
-$a = 154.0; //float
-$b = 1.54E2;
-var_dump($a);
-var_dump($b);
-
-$a = 'Letter from friend'; //string
-var_dump($a);
-
-$bool = false; //boolean
-var_dump($bool);
-var_dump(isset($bool));
-
-$null = null; //nul
-var_dump($null);
-var_dump(isset($null));
-$null = '';
-var_dump(isset($null));
-
-echo '<hr>';
-/*lesson 2*/
-
-$bars1 = array(100, 'псьмо', true); //array
-$meshok = [
-    'кошелек' => 500,
-    'конверт' => 'курсовая работа',
-    'чехол' => false,
-    'барсетка' => [
-        'ipad', 'ipod', ['samsung', 'lenovo']
-    ]
-];
-var_dump($bars1);
-var_dump($meshok);
-
-$obj = new stdClass(); //object
-var_dump($obj);
-$obj->a = 'I am property a';
-var_dump($obj);
-$obj->b = $meshok;
-var_dump($obj);
-
-$f = fopen('text.txt', 'r'); // resource
-var_dump($f);
-
-echo '<hr>';
 /*lesson 3*/
+$a = 4;
+$b = 2;
+
+$c = $a + $b;
+var_dump($c);
+
+$c = $a - $b;
+var_dump($c);
+
+$c = $a * $b;
+var_dump($c);
+
+$c = $a / $b;
+var_dump($c);
+
+$d = 1;
+$c = $d + $a / $b;
+var_dump($c);
+$c = ($d + $a) / $b;
+var_dump($c);
+
+$d += $a; // $d = $d + $a
+var_dump($d);
+
+$d -= $a;
+var_dump($d);
+$d *= $a;
+var_dump($d);
+$d /= $a;
+var_dump($d);
+
+$d++; // $d-- --$d ++$d
+var_dump($d);
+
+$d = 1;
+
+$d += $d++ + ++$d; // $d += 1 + 3; // 7
+var_dump($d);
+
+echo '<hr>';
---
+++ index1.php
@@ -0,0 +1,56 @@
+<?php
+
+/*lesson 1*/
+
+$a = 1; //integer
+$b = 2;
+echo $a + $b . '<br>';
+var_dump($a+$b);
+
+$a = 154.0; //float
+$b = 1.54E2;
+var_dump($a);
+var_dump($b);
+
+$a = 'Letter from friend'; //string
+var_dump($a);
+
+$bool = false; //boolean
+var_dump($bool);
+var_dump(isset($bool));
+
+$null = null; //nul
+var_dump($null);
+var_dump(isset($null));
+$null = '';
+var_dump(isset($null));
+
+echo '<hr>';
+/*lesson 2*/
+
+$bars1 = array(100, 'псьмо', true); //array
+$meshok = [
+    'кошелек' => 500,
+    'конверт' => 'курсовая работа',
+    'чехол' => false,
+    'барсетка' => [
+        'ipad', 'ipod', ['samsung', 'lenovo']
+    ]
+];
+var_dump($bars1);
+var_dump($meshok);
+
+$obj = new stdClass(); //object
+var_dump($obj);
+$obj->a = 'I am property a';
+var_dump($obj);
+$obj->b = $meshok;
+var_dump($obj);
+
+$f = fopen('text.txt', 'r'); // resource
+var_dump($f);
+
+echo '<hr>';
+
+
+
