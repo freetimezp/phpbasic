1c36b1ef836894aa27fbb26e1e4c96373b133ca0
freetimezp <kraftukrnet@ukr.net>
2021-07-04 18:39:58 +0300

    phpbasic 4

--- index.php
+++ index.php
@@ -41,3 +41,30 @@ $d += $d++ + ++$d; // $d += 1 + 3; // 7
 var_dump($d);
 
 echo '<hr>';
+/*lesson 4*/
+
+$language = 'php';
+
+$str = 'I am learning "PHP"';
+var_dump($str);
+$str = 'I am learning $language';
+var_dump($str);
+$str = "I am learning $language";
+var_dump($str);
+$str = 'I am learning ' . $language;
+var_dump($str);
+
+$str_end = '... I know php';
+
+$final = $str . $str_end;
+var_dump($final);
+
+$arr['language'] = 'php';
+
+$str = "I am learning {$arr['language']}";
+var_dump($str);
+
+$final2 = 'Hello ';
+$str2 = 'php';
+$final2 .= $str2;
+var_dump($final2);
