7d1d86ce4b32ce62c842226f10953d219d62aad0
freetimezp <kraftukrnet@ukr.net>
2021-07-07 21:24:32 +0300

    phpbasic 12

--- index.php
+++ index.php
@@ -10,3 +10,30 @@ function foo($num1 = 1, $num2 = 2, $num3 = 3) {
 }
 
 echo foo(50,50,50   );
+
+echo '<hr>';
+/*lesson 12*/
+
+$glob = 'Masha';
+
+function hello($str) {
+    global $glob;
+    echo $str . ' ' . $glob . '<br>';
+}
+
+$hello = 'hello';
+$hello1 = 'hi';
+hello($hello);
+hello($hello1);
+
+$glob = 'Masha';
+
+$foo = function ($str) use($glob) {
+    echo '<br>' . $str . ' ' . $glob , '<br>';
+    $glob = 'Maxim';
+};
+
+$foo('Hello');
+
+echo '<hr>';
+/*lesson 13*/
